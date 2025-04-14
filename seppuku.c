f2a62538dcc71aafe47287c70b71a00492f9bd44
quesadilla <debian.dave@pm.me>
2025-04-13 19:08:23 -1000

    added debug for only printing when I want

--- seppuku.c
+++ seppuku.c
@@ -6,22 +6,25 @@
  */
 
 #define _GNU_SOURCE
-#include <stdio.h>  //needed for printf
-#include <stdlib.h> //needed for atoi and getenv
-#include <unistd.h> //pull sleep and getpid
+#include <stdio.h>  //printf
+#include <stdlib.h> //atoi and getenv
+#include <unistd.h> //sleep and getpid
 #include <signal.h> //handle sigterm
 #include <pthread.h> //creation of threads
+#include <stdbool.h> //bool
 
 static pthread_t timer_thread;
-static int DEFAULT_SECONDS = 0;
+static int DEFAULT_SECONDS = 10;
+static bool DEBUG = true;
 
 // Timer thread function
-void* timer_function(void* arg) 
+void* timer_function() 
 {
     // Sleep for the specified number of seconds
     sleep(DEFAULT_SECONDS);
     
-    printf("\n[seppuku.so] Time limit of %d seconds reached. Terminating process.\n", 
+    //check debug before printing
+    if (DEBUG) printf("\n[seppuku.so] Time limit of %d seconds reached. Terminating process.\n", 
         DEFAULT_SECONDS);
     
     // Send SIGTERM to the current process
@@ -40,23 +43,27 @@ static void setup_seppuku(void)
         DEFAULT_SECONDS = atoi(seppuku_str);
     } else 
     {
-        printf("No time found in environment, reverting to default\n");
-    }    
+        if (DEBUG) printf("[seppuku.so] No time found in environment, reverting to default: %ds.\n", 
+            DEFAULT_SECONDS);
+    }
+
     if (DEFAULT_SECONDS > 0) 
     {
-        printf("[seppuku.so] Setting seppuku to %d seconds\n", DEFAULT_SECONDS);
+        if (DEBUG) printf("[seppuku.so] Setting seppuku to %d seconds\n", 
+            DEFAULT_SECONDS);
         
         // Create a thread to handle the timer
         if (pthread_create(&timer_thread, NULL, timer_function, NULL) != 0) 
         {
-            perror("[seppuku.so] Failed to create timer thread");
+            if (DEBUG) perror("[seppuku.so] Failed to create timer thread");
             return;
         }
         
         // Detach the thread so its resources are freed automatically
         pthread_detach(timer_thread);
     } else {
-        printf("[seppuku.so] Invalid seppuku_SECONDS value: %s\n", seppuku_str);
+        if (DEBUG) printf("[seppuku.so] Invalid seppuku_SECONDS value: %s\n", 
+            seppuku_str);
     }
 } 
 
@@ -67,6 +74,8 @@ static void cleanup_seppuku(void)
     // Cancel the timer thread if it's still running
     if (DEFAULT_SECONDS > 0) 
     {
+        if(DEBUG) printf("[seppuku.so] Process end before timer\n");
+        
         pthread_cancel(timer_thread);
     }
 }
