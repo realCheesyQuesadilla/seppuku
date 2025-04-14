83aea03b883e060fea09b90984ea199c5cffb2a8
quesadilla <debian.dave@pm.me>
2025-04-13 18:46:28 -1000

    formatting and some logic

--- seppuku.c
+++ seppuku.c
@@ -2,27 +2,27 @@
  * seppuku.c - A shared library to terminate a process after a specified time
  *
  * Usage: LD_PRELOAD=./seppuku.so TIME=<seconds> <command>
+ * 
  */
 
 #define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <time.h>
-#include <pthread.h>
-#include <dlfcn.h>
+#include <stdio.h>  //needed for printf
+#include <stdlib.h> //needed for atoi and getenv
+#include <unistd.h> //pull sleep and getpid
+#include <signal.h> //handle sigterm
+#include <pthread.h> //creation of threads
 
 static pthread_t timer_thread;
-static int seppuku_seconds = 0;
+static int DEFAULT_SECONDS = 0;
 
 // Timer thread function
-void* timer_function(void* arg) {
+void* timer_function(void* arg) 
+{
     // Sleep for the specified number of seconds
-    sleep(seppuku_seconds);
+    sleep(DEFAULT_SECONDS);
     
     printf("\n[seppuku.so] Time limit of %d seconds reached. Terminating process.\n", 
-           seppuku_seconds);
+        DEFAULT_SECONDS);
     
     // Send SIGTERM to the current process
     kill(getpid(), SIGTERM);
@@ -32,36 +32,41 @@ void* timer_function(void* arg) {
 
 // Constructor function that will be executed when the library is loaded
 __attribute__((constructor))
-static void setup_seppuku(void) {
-    char* seppuku_str = getenv("seppuku_SECONDS");
-    
-    if (seppuku_str != NULL) {
-        seppuku_seconds = atoi(seppuku_str);
+static void setup_seppuku(void) 
+{
+    char* seppuku_str = getenv("TIME");
+    if (seppuku_str != NULL) 
+    {
+        DEFAULT_SECONDS = atoi(seppuku_str);
+    } else 
+    {
+        printf("No time found in environment, reverting to default\n");
+    }    
+    if (DEFAULT_SECONDS > 0) 
+    {
+        printf("[seppuku.so] Setting seppuku to %d seconds\n", DEFAULT_SECONDS);
         
-        if (seppuku_seconds > 0) {
-            printf("[seppuku.so] Setting seppuku to %d seconds\n", seppuku_seconds);
-            
-            // Create a thread to handle the timer
-            if (pthread_create(&timer_thread, NULL, timer_function, NULL) != 0) {
-                perror("[seppuku.so] Failed to create timer thread");
-                return;
-            }
-            
-            // Detach the thread so its resources are freed automatically
-            pthread_detach(timer_thread);
-        } else {
-            printf("[seppuku.so] Invalid seppuku_SECONDS value: %s\n", seppuku_str);
+        // Create a thread to handle the timer
+        if (pthread_create(&timer_thread, NULL, timer_function, NULL) != 0) 
+        {
+            perror("[seppuku.so] Failed to create timer thread");
+            return;
         }
+        
+        // Detach the thread so its resources are freed automatically
+        pthread_detach(timer_thread);
     } else {
-        printf("[seppuku.so] seppuku_SECONDS environment variable not set. No seppuku will be applied.\n");
+        printf("[seppuku.so] Invalid seppuku_SECONDS value: %s\n", seppuku_str);
     }
-}
+} 
 
 // Destructor function that will be executed when the library is unloaded
 __attribute__((destructor))
-static void cleanup_seppuku(void) {
+static void cleanup_seppuku(void) 
+{
     // Cancel the timer thread if it's still running
-    if (seppuku_seconds > 0) {
+    if (DEFAULT_SECONDS > 0) 
+    {
         pthread_cancel(timer_thread);
     }
 }
